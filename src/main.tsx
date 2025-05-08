49d89c85236c1ec2b58025c8beb2321798ec4fc3
webmateshq <webmatehq@gmail.com>
2025-05-08 14:36:29 -0500

    Agregando el css y ya al fin hppppp

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,10 @@
 import { createRoot } from "react-dom/client";
+import React from "react";
 import App from "./App";
 import "./index.css";
 
-// Add fonts
-const montserrat = document.createElement("link");
-montserrat.rel = "stylesheet";
-montserrat.href = "https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;600;700&display=swap";
-document.head.appendChild(montserrat);
-
-const openSans = document.createElement("link");
-openSans.rel = "stylesheet";
-openSans.href = "https://fonts.googleapis.com/css2?family=Open+Sans:wght@300;400;600&display=swap";
-document.head.appendChild(openSans);
-
-createRoot(document.getElementById("root")!).render(<App />);
+createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
