7f5a2a395a2231b6592276590c4a6f41f7a893e2
dariorealpe1 <dariorealpe54@gmail.com>
2025-09-04 22:03:51 +0200

    fix

--- src/components/home/ConstructionBanner.tsx
+++ src/components/home/ConstructionBanner.tsx
@@ -3,14 +3,14 @@ import { motion } from "framer-motion";
 export default function ConstructionBanner() {
   return (
     <motion.div 
-      className="bg-[#D9A566] text-white text-center py-2 px-4"
+      className="bg-[#D9A566] text-white text-center py-2 px-4 h-12"
       initial={{ y: -50, opacity: 0 }}
       animate={{ y: 0, opacity: 1 }}
       transition={{ duration: 0.5, ease: "easeOut" }}
     >
-      <p className="font-montserrat font-semibold">
+      {/* <p className="font-montserrat font-semibold">
         🚧 Pardon Our Dust, We're Crafting Something Special. Opening Summer 2025! 🚧
-      </p>
+      </p> */}
     </motion.div>
   );
 }
